8da84674d0011fafb53f9994565347065fd3a406
Markus Stuppnig <markus@stuppnig.net>
2024-02-06 17:29:52 +0100

    removes unnecessary import statement

--- components/Recommendations/Recommendations.tsx
+++ components/Recommendations/Recommendations.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import SectionHeading from "../-General/SectionHeading";
-import { motion } from "framer-motion";
 import { recommendationsData } from "./recommendationsData";
 import Image from "next/image";
 import Link from "next/link";
