91117553a6607d98eb40492541e9a2eded92a206
Markus Stuppnig <markus@stuppnig.net>
2024-02-06 18:10:23 +0100

    fixes iOS bg scroll bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,8 @@ export default function RootLayout({
 
   return (
     <html lang="en" className='scroll-smooth overflow-scroll' style={{scrollBehavior:'smooth'}}>
-      <body style={{background: "url('svgviewer-output.svg') no-repeat center center fixed", backgroundSize: 'cover'}}>
+      <body>
+        <div className="bg-black" style={{position: "fixed", top: "", left: "0", width: "100%", height: "100%", background: "url('bg/background_v3.png')", backgroundPosition: "bottom left", backgroundSize: 'cover', backgroundRepeat: "no-repeat", zIndex: "-1000", opacity: "1.0"}} />
         <Navbar />
         <div className="overflow-x-hidden">
           <div className={`${inter.className} bg-gray-50 text-gray-950 pt-28 sm:pt-36`}>
