b8cca6eb1e6baf5aa3b0de87493d6faf7c1c3b1f
Markus Stuppnig <markus@stuppnig.net>
2024-02-07 23:20:56 +0100

    projects shadow

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -9,7 +9,7 @@ export default function Projects() {
   return (
 	<section id="projects" className="mt-[5rem]">
 		<div className="grid grid-cols-12 h-[30rem]">
-			<div className="col-span-12 md:col-start-4 md:col-span-9 bg-secondary py-[10rem] md:rounded-l-3xl grid grid-cols-12">
+			<div className="col-span-12 md:col-start-4 md:col-span-9 bg-secondary shadow-2xl py-[10rem] md:rounded-l-3xl grid grid-cols-12">
 				<div className="text-center col-start-2 col-span-10 md:col-start-3 md:col-span-5 flex flex-col items-center justify-center">
 					<SectionHeading additional="font-semibold mb-5" textColor="text-mywhite">Projects</SectionHeading>
 					<h4 className="text-2xl text-mywhite">Here are some of my projects</h4>
@@ -18,7 +18,7 @@ export default function Projects() {
 		</div>
 		<SectionDivider />
 		<div className="grid grid-cols-3">
-			<div className="col-span-3 md:col-span-2 bg-secondary text-mywhite py-[6rem] grid grid-cols-12 md:rounded-r-3xl">
+			<div className="col-span-3 md:col-span-2 bg-secondary shadow-2xl text-mywhite py-[6rem] grid grid-cols-12 md:rounded-r-3xl">
 				<Link href={projectsData[0].link} target="_blank" className="col-start-2 md:col-start-3 col-span-10 md:col-span-8 flex flex-col">
 					<SectionHeading additional="font-semibold mb-5" textColor="text-mywhite">{projectsData[0].title}</SectionHeading>
 					<div className="w-full grid grid-cols-9">
@@ -31,7 +31,7 @@ export default function Projects() {
 			</div>
 		</div>
 		<div className="grid grid-cols-3 mt-[10rem]">
-			<div className="col-span-3 md:col-start-2 md:col-span-2 bg-secondary text-mywhite py-[6rem] grid grid-cols-12 md:rounded-l-3xl">
+			<div className="col-span-3 md:col-start-2 md:col-span-2 bg-secondary shadow-2xl text-mywhite py-[6rem] grid grid-cols-12 md:rounded-l-3xl">
 				<Link href={projectsData[1].link} target="_blank" className="col-start-2 md:col-start-3 col-span-10 md:col-span-8 flex flex-col">
 					<SectionHeading additional="font-semibold mb-5" textColor="text-mywhite">{projectsData[1].title}</SectionHeading>
 					<div className="w-full grid grid-cols-9">
