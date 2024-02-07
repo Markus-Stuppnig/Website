757d06900fbbc304f2b52814c736599274259b14
Markus Stuppnig <markus@stuppnig.net>
2024-02-07 16:57:03 +0100

    text update

--- components/Introduction/Introduction.tsx
+++ components/Introduction/Introduction.tsx
@@ -8,7 +8,7 @@ export default function Introduction() {
 		<div className="flex flex-col justify-center items-center bg-secondary text-mywhite pt-[5rem] py-[15rem]">
 			<div className="flex flex-col w-11/12 md:w-2/3 max-w-[60rem] text-center">
 				<SectionHeading additional="font-semibold mb-7">Hi, I’m Markus. Nice to meet you.</SectionHeading>
-				<h4 className="text-2xl tracking-wider leading-9">Since my early childhood, I have been immersed in coding. Solving complex problems and appreciating the simplicity of the solutions was truly fascinating for me. I was always good in math and decided to pursue my hobby in a technical college, an ISCED 5 school, this may have been the best decision I ever made. Having returned from my exchange semester in the United States, I will continue my education and simultaneously gain work experience. The technical topics I am most passionate about are cloud computing, cybersecurity, and data science.</h4>
+				<h4 className="text-2xl tracking-wider leading-9">Since my early childhood, I have been immersed in coding. Solving complex problems and appreciating the simplicity of the solutions was truly fascinating for me. I was always good in math and decided to pursue my hobby in a technical college, an ISCED 5 school. This may have been the best decision I ever made. Having returned from my exchange semester in the United States, I will continue my education and simultaneously gain work experience. The technical topics I am most passionate about are cloud computing, cybersecurity, and data science.</h4>
 			</div>
 		</div>
 		<div className="-mt-[10rem] flex flex-col w-full justify-center items-center">
