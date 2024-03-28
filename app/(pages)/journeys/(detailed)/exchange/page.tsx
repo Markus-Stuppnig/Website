483dcc0bde0a360bce8112741ba60062ff623c69
Markus-Stuppnig <markus@stuppnig.net>
2024-03-28 20:51:38 +0100

    Fix: Escape quote and apostrophe

--- app/(pages)/journeys/(detailed)/exchange/page.tsx
+++ app/(pages)/journeys/(detailed)/exchange/page.tsx
@@ -7,15 +7,15 @@ export default function Home() {
 
 		<h3>Decision</h3>
 		<p>
-			When I was about 13 years old, youtube recommended me a video of a girl, talking about her exchange semester. Unfortunately I couldn't find her again. She seemed incredible happy for experiencing such a cool time and kind of made me crave that too. Within the next few weeks I've seen more and more videos of teens sharing their memories - mostly positive.
+			When I was about 13 years old, youtube recommended me a video of a girl, talking about her exchange semester. Unfortunately I couldn&apos;t find her again. She seemed incredible happy for experiencing such a cool time and kind of made me crave that too. Within the next few weeks I&apos;ve seen more and more videos of teens sharing their memories - mostly positive.
 			Surprisingly on one day, my math teacher in lower secondary was talking about a carrier event that would take place in our school the very next weekend. So I said fuck it I am going there. My dad and I went there and soon found out about Education First (short EF), an organization that specializes in exchange years and language trips.
-			We gave them our contact details, and almost forgot about it. But then, 2 years later they CALLED us - "Is Markus still interested in an exchange semester?" - And Fuck yeah I was.
+			We gave them our contact details, and almost forgot about it. But then, 2 years later they CALLED us - &quot;Is Markus still interested in an exchange semester?&quot; - And Fuck yeah I was.
 		</p>
 
 		<h3>Preperations</h3>
 		<p>
-			Around one year before my planned trip, the preperations started. There was in infinite amount of documents that had to be filled out, vaccines I had to get and health test I had to do. After these exhausting 12 months I was sitting at home, my luggage already packed up, going to sleep to be ready for my big day. The day I'd cross the ocean and explore a new continent - at least that's what it felt like.
-			After _Waking Up_ I saw the text from my hostdad Ben 'Hope you got some sleep' - not at all - no sleep for me on the recipe.
+			Around one year before my planned trip, the preperations started. There was in infinite amount of documents that had to be filled out, vaccines I had to get and health test I had to do. After these exhausting 12 months I was sitting at home, my luggage already packed up, going to sleep to be ready for my big day. The day I&apos;d cross the ocean and explore a new continent - at least that&apos;s what it felt like.
+			After _Waking Up_ I saw the text from my hostdad Ben &apos;Hope you got some sleep&apos; - not at all - no sleep for me on the recipe.
 		</p>
 
 		<h3>Flight</h3>
